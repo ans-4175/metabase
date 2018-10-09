377001c78f800a8252504c4e463f67115f9ecef5
Anshori <ans4175@gmail.com>
2018-10-10 05:41:26 +0700

    fix(carousel): add checkRenderable and default column image to null

--- frontend/src/metabase/visualizations/visualizations/ImageCarousel.jsx
+++ frontend/src/metabase/visualizations/visualizations/ImageCarousel.jsx
@@ -4,14 +4,9 @@ import React, { Component } from "react";
 import styles from "./ImageCarousel.css";
 import ImageGallery from 'react-image-gallery';
 
-import {
-  getDefaultColumns,
-  getOptionFromColumn,
-} from "metabase/visualizations/lib/settings";
+import { getOptionFromColumn } from "metabase/visualizations/lib/settings";
 
-import {
-  ChartSettingsError,
-} from "metabase/visualizations/lib/errors";
+import { ChartSettingsError } from "metabase/visualizations/lib/errors";
 
 import type { VisualizationProps } from "metabase/meta/types/Visualization";
 
@@ -29,19 +24,11 @@ export default class ImageCarousel extends Component {
 
   _carousel: ?HTMLElement;
 
-  static isSensible(cols, rows) {
-    return rows.length === 1 && cols.length === 1;
-  }
-
-  static checkRenderable([{ data: { cols, rows } }]) {
-    // NOTE: image carousel validation for rendering postponed due my incompetent understanding yet
-  }
-
   static settings = {
     "graph.series":{
       title: `Image Columns`,
       widget: "fields",
-      getDefault: (series, vizSettings) => getDefaultColumns(series).metrics,
+      getDefault: (series, vizSettings) => [null],
       persistDefault: true,
       getProps: ([{ card, data }], vizSettings) => {
         const options = data.cols.map(getOptionFromColumn);
@@ -54,6 +41,15 @@ export default class ImageCarousel extends Component {
       useRawSeries: true,
     }
   };
+
+  static checkRenderable([{ data: { cols, rows } }], settings) {
+    if (!settings["graph.series"][0]) {
+      throw new ChartSettingsError(
+        `Please select image column in the chart settings.`,
+        "Data",
+      );
+    }
+  }
 
   render() {
     let {
